fb1368d3b28eefa2d3fdb33e85e7dfe902465a62
Kkuma125 <kumar95530@gmail.com>
2023-03-21 21:44:05 +0530

    updated code 1

--- src/test/java/LamdaTest/SimpleFormDemoTest.java
+++ src/test/java/LamdaTest/SimpleFormDemoTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
